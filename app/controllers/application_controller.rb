17333cec90d39a3a607822099f4d9a809720c85c
John Doe <digitalysin@yahoo.co.id>
2012-02-07 08:51:04 +0700

    fix all

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,6 @@ class ApplicationController < ActionController::Base
       Visitor.check_visitor request.env['REMOTE_ADDR'], request.env['PATH_INFO']
       @statistic = Statistic.first
       @site_configuration = SiteConfiguration.first 
-      unless Statistic.today?
-        Statistic.update_advertisements
-      end      
+      Statistic.update_advertisements unless Statistic.today?
     end
 end
